3c5b6eb87a8c13f58a02629b09e15a27fa16c857
eradicador <janisepinto@yahoo.com>
2020-08-25 00:34:06 -0400

    added video backgrounds to login and register pages

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -5,6 +5,7 @@ import { useSelector, useDispatch } from "react-redux";
 import LoginForm from "../../components/LoginForm";
 import { loginUser } from "../../actions/authActions";
 import { Link } from "react-router-dom";
+import videobg from "../../images/sport_intro.mp4"
 
 
 const Login = (props) => {
@@ -30,6 +31,19 @@ const Login = (props) => {
 
     return (
         <div className="form-container">
+            <video autoPlay loop muted
+                style={{
+                    position: "absolute",
+                    width: "100%",
+                    left: "50%",
+                    top: "50%",
+                    height: "100%",
+                    objectFit: "cover",
+                    transform: "translate(-50%, -50%)",
+                }}
+                >
+                <source src={videobg} type="video/mp4" />
+            </video>
             <Header as='h2' secondary="true" textAlign='center'>
                 Login to your account
                 </Header>
